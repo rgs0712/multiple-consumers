3ecf94bbf0ccd8e342de33602817080e13ce02dd
rgs <rgarciasantanna@gmail.com>
2025-05-10 12:29:58 -0300

    first commit

---
+++ src/main/java/com/rgs/multipleconsumer/MultipleConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.rgs.multipleconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleConsumerApplication.class, args);
+    }
+
+}
